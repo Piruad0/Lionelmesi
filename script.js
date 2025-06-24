374d40596d3d1b3c3c5e38a48b48064d7531e554
Piruad0 <augusto.reque@escola.pr.gov.br>
2025-06-24 12:19:57 +0000

    terceiro lepo

--- script.js
+++ script.js
@@ -5,25 +5,7 @@ botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 function mostraPalavrasChave() {
     const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
-    const palavrasChave = processaTexto(texto);
+    const palavras = texto.split(" ");
 
-    campoResultado.textContent = palavrasChave.join(", ");
-}
-
-function processaTexto(texto) {
-    let palavras = texto.split(/\P{L}+/u);
-
-    let frequencias = {};
-    for (let i of palavras) {
-        frequencias[i] = 0;
-        for (let j of palavras) {
-            if (i == j) {
-                frequencias[i]++;
-            }
-        }
-    }
-
-    console.log(frequencias);
-
-    return palavras;
+    campoResultado.textContent = palavras.join(", ");
 }
